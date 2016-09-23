a1a19e74b17b9659e000ebdfd2016251ca2bbfd7
manish-utm <manishchauhan1621995@gmail.com>
2016-09-23 21:14:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('profile1', function(req, res) {
+app.get('/profile1', function(req, res) {
     res.send('Profile will get responded here');
 });
 app.get('/ui/style.css', function (req, res) {
