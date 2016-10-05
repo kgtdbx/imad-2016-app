ce648942923c2e3c7399209e65f37323f784fcf3
manish-utm <manishchauhan1621995@gmail.com>
2016-10-06 04:55:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,52 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var Developer2={
+    title:'developer-profile 2',
+    heading:'Devepoler-profile 2',
+    date:'oct 6, 2016',
+    content:
+    `<p>THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.</p>
+         <p>THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.</p>
+         <p>THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.</p>`
+};
+function createtemplate(data)
+{
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    
+    var htmltemplate=`
+<html>
+    <head>
+        <title>${title}</title>
+        <meta name="viewpoint" content="width=device-width initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet"/>
+        <style>
+       </style>
+    </head>
+    <body>
+        <a href="/"><b>HOME</b></a>
+        <hr/>
+        <h1><u>${heading}</u></h1>
+        
+        <div class="deco">
+       ${content}
+        </div>
+        <div>${date}</div>
+    </body>
+    
+</html>`
+return htmltemplate;
+
+}
+
+
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -12,8 +58,10 @@ app.get('/developer-profile', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'developer-profile.html'));
     
 });
-app.get('/developer-profile2', function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'developer-profile2.html'));
+//app.get('/developer-profile2', function(req, res) {
+  //res.sendFile(path.join(__dirname, 'ui', 'developer-profile2.html'));
+  app.get('/developer-profile2', function(req, res) {
+  res.send(createtemplate(developer2));
     
 });
 app.get('/developer-profile3', function(req, res) {
