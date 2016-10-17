e5f11ec7968df064eb9fdb395247e1bc4654af56
manish-utm <manishchauhan1621995@gmail.com>
2016-10-17 11:45:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,66 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var p_two ={
+  title :'D2',
+  heading :'Developer2',
+  date : 'oct 17',
+  content : ` <p>THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.</p>
+                 <p>THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.</p>
+                 <p>THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.THIS IS A VERY BAD BOY.</p> `
+};
+function createtemp(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    var htmltemp =`
+            <html>
+            <head>
+                    <title>${title}</title>
+                    <meta name="viewpoint" content="width=device-width initial-scale=1" />
+                    <link href="/ui/style.css" rel="stylesheet"/>
+            </head>
+            <body>
+                    <a href="/"><b>HOME</b></a>
+                    <hr/>
+                    <h1><u>${heading}</u></h1>
+                    
+                    <div class="deco">
+                    ${content}
+                    </div>
+                    <div>${date}</div>
+            </body>
+            
+        </html>
+    
+     `;
+     return htmltemp;
+ }
+
+
+
+app.get('/developer-profile2', function(req, res) {
+res.send(createtemp(p_two));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -12,9 +72,9 @@ app.get('/developer-profile', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'developer-profile.html'));
     
 });
-app.get('/developer-profile2', function(req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'developer-profile2.html'));
-});
+//  app.get('/developer-profile2', function(req, res) {
+//  res.sendFile(path.join(__dirname, 'ui', 'developer-profile2.html'));
+//  });
  
 app.get('/developer-profile3', function(req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'developer-profile3.html'));
