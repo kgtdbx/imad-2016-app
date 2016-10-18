a8d5b4382bc77ce76e1569e8d3aa59493e15a416
manish-utm <manishchauhan1621995@gmail.com>
2016-10-18 23:45:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,15 @@ console.log('MY WORK IS ALMOST DONE');
 //var element= document.getElementById('main-text');
 //element.innerHTML='NEW VALUE';
 var img=document.getElementById('madi');
-img.onmouseover= function()
-                        {
-                          img.style.marginLeft= '100px';  
-                        };
+function bigImg(x) {
+    x.style.height = "64px";
+    x.style.width = "64px";
+}
 
+function normalImg(x) {
+    x.style.height = "32px";
+    x.style.width = "32px";
+}
+img.onmouseover= bigImg(img);
+img.onmouseout= bigImg(img);
+                        
