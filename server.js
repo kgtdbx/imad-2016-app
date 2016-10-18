a96e16270444bf0bc56615a3f35a294239392ad0
manish-utm <manishchauhan1621995@gmail.com>
2016-10-19 02:22:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,8 +56,8 @@ res.send(createtemp(profiles[pName]));
 });
 
 var counter = 0;
-app.get('/counter',function(req,res) {
-    counter=counter + 1;
+app.get('/counter', function(req,res) {
+    counter = counter + 1;
     res.send(counter.toString());
   });
 
