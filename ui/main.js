812c5c27c2dc0cec333274725dacbb13c8bac56c
manish-utm <manishchauhan1621995@gmail.com>
2016-10-18 23:30:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 console.log('MY WORK IS ALMOST DONE');
-var element= document.getElementById('main-text');
-element.innerHTML='NEW VALUE';
+//var element= document.getElementById('main-text');
+//element.innerHTML='NEW VALUE';
 var img=document.getElementById('madi');
 img.onclick= function()
                         {
